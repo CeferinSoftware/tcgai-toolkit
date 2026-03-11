9939cf584d5b9b10ad25daf0989a3a9a3199cc44
CeferinSoftware <newshellweb@gmail.com>
2026-03-11 12:59:46 +0100

    chore: bump version to 0.4.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="tcgai-toolkit",
-    version="0.3.0",
+    version="0.4.0",
     author="TCG AI PRO",
     description="Python toolkit for TCG card image analysis",
     long_description=long_description,
